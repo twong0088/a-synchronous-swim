0ce4571708e15dce1c50b8bfafcb150f74281056
Vikram Pal <vikramsonupal@gmail.com>
2020-10-23 17:29:12 -0700

    Worked through random directions

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -14,7 +14,27 @@ module.exports.initialize = (queue) => {
 
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
+
   res.writeHead(200, headers);
-  res.end();
+
+  if (req.method === 'OPTIONS') {
+    console.log(res);
+    res.end();
+  }
+
+  if (req.method === 'GET') {
+
+    // Create an options array
+    //helper function
+      // Select a random option to be passed into res.end();
+
+    res.end(randomDirection());
+  }
   next(); // invoke next() at the end of a request to help with testing!
 };
+
+var randomDirection = () => {
+  var directions = ['up', 'down', 'left', 'right'];
+  var index = Math.floor(Math.random() * 4);
+  return directions[index];
+}
